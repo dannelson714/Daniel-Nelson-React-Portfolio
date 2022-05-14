da901ad98a5a8b30553509b120dd018137c1eb11
Daniel Nelson <dan.s.nelson@gmail.com>
2022-05-14 15:44:43 -0400

    Created and populated Footer section of page

--- components/PortfolioContainer.js
+++ components/PortfolioContainer.js
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+import Footer from './Footer';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About Me');
@@ -27,6 +28,7 @@ export default function PortfolioContainer() {
     <div>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
+      <Footer />
     </div>
   );
 }
