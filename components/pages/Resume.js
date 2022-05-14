742a00951ed8a44f91cd05b4caeeb6164b0c501b
Daniel Nelson <dan.s.nelson@gmail.com>
2022-05-14 09:56:59 -0400

    Completed Resume page

--- components/pages/Resume.js
+++ components/pages/Resume.js
@@ -3,19 +3,15 @@ import React from 'react';
 export default function Resume() {
   return (
     <div>
-      <h1>Resume</h1>
+      <h1>Please <a href="../../assets/CV.pdf">view</a> my resume.</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+      <ul>
+        <li>Dynamic & Reactive Web Development | React.js, JavaScript, HTML5, CSS3, Bootstrap</li>
+        <li>Data-Driven Computing | jQuery, RESTful APIs, JSON, AJAX</li>
+        <li>Back-End Development | Node.js, Express.js, MySQL, Sequelize, NoSQL (MongoDB), Insomnia Scripting | Python, Linux bash/shell, Unit testing</li>
+        <li>Progressive Web Application | Asynchronous & Distributed Computing</li>
+        <li>General Computer Science | Data Structures, Algorithms, Resource Management</li>
+      </ul>
       </p>
     </div>
   );
