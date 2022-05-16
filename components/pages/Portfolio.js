41c120aa5bd6e60861a0afa3d351bd2ccc16cc05
Daniel Nelson <dan.s.nelson@gmail.com>
2022-05-16 18:34:27 -0400

    Created array of project objects, established functionality for project and portfolio page components.

--- components/pages/Portfolio.js
+++ components/pages/Portfolio.js
@@ -1,24 +1,57 @@
 import React from 'react';
+import Project from './Project';
+
+const projects = [
+  {
+      id: 1,
+      name: "Pandemic Blight",
+      img: "../../assets/65581C0C-17DB-4BDE-ADBA-BE742076CC08_1_201_a.jpeg",
+      githubURL: "https://github.com/dannelson714/Pandemic-Blight",
+      deployedURL: "https://dwarrick91.github.io/Pandemic-Blight/",
+  },
+  {
+    id: 2,
+    name: "Weather Dashboard",
+    img: "../../assets/B147618C-64A7-4AEE-9F96-4AE2D2767DD7_1_201_a.jpeg",
+    githubURL: "https://github.com/dannelson714/Weather-Dashboard",
+    deployedURL: "https://dannelson714.github.io/Weather-Dashboard",
+},
+{
+  id: 3,
+  name: "Notetaker App",
+  img: "../../assets/56D110C2-B28A-42A3-AD9E-8807C032CB02_1_105_c.jpeg",
+  githubURL: "https://github.com/dannelson714/Note-Taker",
+  deployedURL: "https://serene-shore-33969.herokuapp.com/",
+},
+{
+  id: 4,
+  name: "Film Fan",
+  img: "../../assets/filmfan.png",
+  githubURL: "https://github.com/dannelson714/film-fan",
+  deployedURL: "https://floating-depths-94622.herokuapp.com/",
+},
+{
+  id: 5,
+  name: "Work Day Scheduler",
+  img: "../../assets/Screen Shot 2022-03-24 at 4.23.17 PM.jpeg",
+  githubURL: "https://github.com/dannelson714/Work-Day-Scheduler",
+  deployedURL: "https://dannelson714.github.io/Work-Day-Scheduler",
+},
+{
+  id: 6,
+  name: "Music Theory Quiz",
+  img: "../../assets/Screen Shot 2022-03-22 at 4.06.20 PM.jpeg",
+  githubURL: "https://github.com/dannelson714/Music-Theory-Quiz",
+  deployedURL: "https://dannelson714.github.io/Music-Theory-Quiz",
+},
+
+]
 
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <Project id={projects.id} name={projects.name} img={projects.img} githubURL={projects.githubURL} deployedURL={projects.deployedURL} />
     </div>
   );
 }
--- components/pages/Project.js
+++ components/pages/Project.js
@@ -3,20 +3,19 @@ import React from "react";
 export default function Project(props) {
   return (
     <ul className="list-group">
-        {props.project.map(item => (
+        {props.projects.map(item => (
             <li className="list-group-item" key={item.id}>
                 <div className="container">
                     <div className="card">
                         <img
                             className="card-img-top"
-                            src={props.img}
+                            src={item.img}
                             alt="Card cap"
                         />
                         <div className="card-body">
                             <h5 className="card-title">Name: {item.name}</h5>
                             <a href={item.githubURL} className="card-text">Github Repository</a>
                             <a href={item.deployedURL} className="card-text">Deployed App</a>
-                            <p className="card-text">ID: {props.id}</p>
                         </div>
                     </div>
                 </div>
